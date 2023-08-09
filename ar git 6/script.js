eea87c30474b96299ac870b06a17033652fcf409
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-09 07:12:17 -0700

    Add files via upload

---
+++ ar git 6/script.js
@@ -0,0 +1,11 @@
+// let text = "Hello planet earth, you are a great planet.";
+// let result = text.lastIndexOf("planet");
+
+// document.getElementById("demo").innerHTML = result;
+
+
+
+let text = "Hello planet earth, you are a great planet.";
+let result = text.lastIndexOf("Planet");
+
+document.getElementById("demo").innerHTML = result;
